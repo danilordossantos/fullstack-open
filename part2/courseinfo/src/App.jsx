d4b4f565aade5ea8df7dbca43a659a188a4689dd
Danilo Ribeiro dos Santos <danr.dossantos.work@gmail.com>
2026-04-15 23:56:03 -0300

    version 1
    props

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />      
+    </div>
+
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <article>      
+      <Header course={course} />
+      <Content course={course}/>
+    </article>
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <article>
+      {course.parts.map(part => (<Part key={part.id} part={part}/>))}
+    </article>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <article>
+      <p>{part.name} {part.exercises}</p>
+    </article>
+  )
+}
+
+export default App
